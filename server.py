b96bb548f5198052215a2d447c597354f812c1bc
nate000088@gmail.com <nate000088@gmail.com>
2017-06-15 14:13:25 -0700

    wall, like facebook's comment wall, assignment

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
-import re, bcrypt
+from datetime import datetime
+import re, bcrypt,random
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
@@ -16,12 +17,41 @@ mysql = MySQLConnector(app, 'registerdb')#change database for appropriate functi
 
 @app.route('/')
 def index():
+	session['css']=random.randrange(0,10000000)
+	usrmsg = mysql.query_db("SELECT * FROM users JOIN messages ON messages.user_id = users.id")
 	msg=''
 	if('regmsg' in session):
 		msg=session['regmsg']
-		session.pop('regmsg')
-	print mysql.query_db("SELECT * FROM users")
-	return render_template('index.html',message=msg)
+	return render_template('index.html',message=msg,user_messages=usrmsg)
+
+@app.route('/wall')
+def wall():
+	session['css']=random.randrange(0,10000000)
+	usrmsg = mysql.query_db("SELECT * FROM users JOIN messages ON messages.user_id = users.id")
+	msg=''
+	if('regmsg' in session):
+		msg=session['regmsg']
+
+	for message in usrmsg:
+		message['commentlist']=mysql.query_db("SELECT * FROM comments JOIN users ON users.id = comments.user_id WHERE message_id = "+str(message['id']))
+
+	print usrmsg
+
+	return render_template('index.html', message=msg,user_messages=usrmsg)
+
+@app.route('/newmsg', methods=['POST'])
+def newmsg():
+	if request.form['newmsg'] and 'id' in session:
+		mysql.query_db('INSERT INTO messages (user_id, message, created_at, updated_at) VALUES ('+str(session['id'])+',"'+request.form['newmsg']+'", NOW(), NOW())')#scrub for quotes later
+	return redirect('/')
+
+@app.route('/newcmnt', methods=['POST'])
+def newcmnt():
+	if request.form['newcmnt'] and 'id' in session:
+		mysql.query_db('INSERT INTO comments (message_id, user_id, comment, created_at, updated_at) VALUES ('+request.form['msgnum']+','+str(session['id'])+',"'+request.form['newcmnt']+'", NOW(), NOW())')#scrub for quotes later
+	return redirect('/wall')
+
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -75,12 +105,19 @@ def login():
 			return render_template('login.html')
 		hashch = bcrypt.hashpw(request.form['pswd'].encode('UTF_8'),passchk[0]['salt'].encode('UTF_8')) 
 		if hashch == passchk[0]['password']:
-			session['regmsg']='login successful! Welcome '+passchk[0]['first_name']+' '+passchk[0]['last_name']
-			return redirect('/')
+			session['regmsg']='Welcome '+passchk[0]['first_name']+' '+passchk[0]['last_name']
+			session['id']=passchk[0]['id']
+			return redirect('/wall')
 		else:
 			flash('Username or password incorrect!')
 			return render_template('login.html')
 	return render_template('login.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+	session.pop('regmsg')
+	session.pop('id')
+	return redirect('/')
 
 
 '''
